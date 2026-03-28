440e829802f53993ff0aed9378d975a25f78042b
sithum <sithumdinujaya9@gmail.com>
2026-03-28 11:39:57 +0530

    Final Next.js POS system code

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -30,6 +30,8 @@ async function connectDB() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
+      serverSelectionTimeoutMS: 5000,
+      connectTimeoutMS: 10000,
     };
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
